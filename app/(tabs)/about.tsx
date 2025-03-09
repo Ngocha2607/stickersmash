49966cfc31ac867dc49441dc3d1b985612de5c35
Ngocha2607 <ngocha2k.ln@gmail.com>
2025-03-09 22:59:50 +0700

    Update assets and remove unused components; add new ImageViewer and AboutScreen

---
+++ app/(tabs)/about.tsx
@@ -0,0 +1,23 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function AboutScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>About Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#25292e",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+});
